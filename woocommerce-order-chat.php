4f370f24732ab98045308fc90f51ac2d7248350b
RutvaPrajapati <prajapati.rutva06@gmail.com>
2026-03-14 22:07:19 +0530

    Update woocommerce-order-chat.php

--- woocommerce-order-chat.php
+++ woocommerce-order-chat.php
@@ -19,16 +19,10 @@ add_action( 'before_woocommerce_init', function() {
     }
 });
 
-/**
- * Load plugin files
- */
-function wcoc_load_plugin() {
-    require_once WCOC_PATH . 'includes/class-wcoc-db.php';
-    require_once WCOC_PATH . 'includes/wcoc-admin.php';
-    require_once WCOC_PATH . 'includes/wcoc-ajax.php';
-    require_once WCOC_PATH . 'includes/wcoc-frontend.php';
-}
-add_action( 'plugins_loaded', 'wcoc_load_plugin' );
+require_once WCOC_PATH . 'includes/class-wcoc-db.php';
+require_once WCOC_PATH . 'includes/wcoc-admin.php';
+require_once WCOC_PATH . 'includes/wcoc-ajax.php';
+require_once WCOC_PATH . 'includes/wcoc-frontend.php';
 
 // Create Table
 register_activation_hook( __FILE__, ['WCOC_DB', 'create_table'] );
